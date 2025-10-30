5e42ac64911d1e091c9b1ae65fd0e66bf3ad1057
Sharick Hernandez <snhernandez79@ucatolica.edu.co>
2025-10-30 13:23:41 -0500

    database terminada

--- database.py
+++ database.py
@@ -2,3 +2,7 @@ from sqlmodel import SQLModel, create_engine
 
 DATABASE_URL = "sqlite:///biblioteca_api.db"
 engine = create_engine(DATABASE_URL, echo=True)
+from sqlmodel import SQLModel, create_engine
+
+def crear_db():
+    SQLModel.metadata.create_all(engine)
